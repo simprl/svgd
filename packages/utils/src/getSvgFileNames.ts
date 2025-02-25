412b188225a0a2159ccef6e6b2d156cf03b45dd2
anatoliisa <pt.ant.sam@gmail.com>
2025-02-25 16:15:17 +0200

    fix getSvgFileNames

--- packages/utils/src/getSvgFileNames.ts
+++ packages/utils/src/getSvgFileNames.ts
@@ -1,9 +1,9 @@
 import { readdirSync } from "fs";
-import path from "path";
+import { resolve, join } from "path";
 
 export function getSvgFileNames(dir: string): string[] {
-    const entries = readdirSync(path.resolve(dir), { withFileTypes: true, recursive: true });
+    const entries = readdirSync(resolve(dir), { withFileTypes: true, recursive: true });
     return entries
         .filter((entry) => /\.(svg)$/i.test(entry.name) && !entry.isDirectory())
-        .map(({ name, parentPath }) => path.join(parentPath, name))
+        .map(({ name, parentPath }) => join(parentPath, name))
 }
